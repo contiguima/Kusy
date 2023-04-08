2e14e85c8a174749fba692a2cacfa22b07bd48ae
Constanza Guimaraez <contiguima@gmail.com>
2023-04-08 18:27:02 -0300

    Problemas de responsive solucionados, añadido modulo COMPRAR y Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Comprar from './pages/Comprar';
 import Landing from './pages/Landing';
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+     <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/comprar" element={<Comprar />} />
+        
+      </Routes>
      
     </div>
   );
---
+++ src/pages/Comprar.jsx
@@ -0,0 +1,12 @@
+const Comprar = () => {
+    return (
+      <>
+        <div >
+          <h1>¡COMPRA!</h1>
+        </div>
+      </>
+    );
+  };
+  
+  export default Comprar;
+  
